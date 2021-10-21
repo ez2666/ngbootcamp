4697dd1a4f20c3a173bbb7c38de6291211c9f329
ez2666 <ez2666@yandex.com>
2021-10-21 21:46:34 +0800

    2 files modified, 4 added

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -1,35 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
-import { ContactService } from 'src/app/services/contact.service';
+import { ContactService } from '../../services/contact.service';
 import { Contact } from '../../models/contact';
 
 @Component({
-    selector: 'contact-detail',
-    templateUrl: './detail.component.html',
-    styleUrls: ['./detail.component.css'],
+  selector: 'contact-detail',
+  templateUrl: './detail.component.html',
+  styleUrls: ['./detail.component.css'],
 })
 export class DetailComponent implements OnInit {
-    contact?: Contact;
+  contact?: Contact;
 
-    constructor(private service: ContactService, private route: ActivatedRoute, private location: Location) { }
+  constructor(
+    private service: ContactService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
 
-    ngOnInit() {
-        this.getContact();
-    }
+  ngOnInit() {
+    this.getContact();
+  }
 
-    getContact() {
-        const id = Number(this.route.snapshot.paramMap.get('id')).toString();
-        this.service.getContact(id)
-            .subscribe(c => this.contact = c);
-    }
+  getContact() {
+    const id = Number(this.route.snapshot.paramMap.get('id')).toString();
+    this.service.getContact(id).subscribe((c) => (this.contact = c));
+  }
 
-    updateContact(contact: Contact) {
-        this.service.updateContact(contact).subscribe(i => {
-        });
-    }
+  updateContact(contact: Contact) {
+    this.service.updateContact(contact).subscribe((i) => {});
+  }
 
-    goBack() {
-        this.location.back();
-    }
+  goBack() {
+    this.location.back();
+  }
 }
