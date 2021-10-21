6eeee52535b482d20287e3dba761f9f0d3f368ec
ez2666 <ez2666@yandex.com>
2021-10-21 21:49:43 +0800

    2 files modified

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,44 +1,49 @@
-import { Component, OnInit } from "@angular/core"
-import { Contact } from "../../models/contact";
-import { ContactService } from "../../services/contact.service";
+import { Component, OnInit } from '@angular/core';
+import { Contact } from '../../models/contact';
+import { ContactService } from '../../services/contact.service';
 
 @Component({
-    selector: "contact-list",
-    templateUrl: "./list.component.html",
-    styleUrls: ["./list.component.css"],
+  selector: 'contact-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
-    contacts: Contact[] = [];
+  contacts: Contact[] = [];
 
-    constructor(private contactService: ContactService) { }
+  constructor(private contactService: ContactService) {}
 
-    ngOnInit(): void {
-        this.getContacts();
+  ngOnInit(): void {
+    this.getContacts();
+  }
+
+  getContacts(): void {
+    // TODO: 1. Omit those contacts without phone number when display list
+    // 2. Don't refresh when contacts are not changed
+    this.contactService.getContacts().subscribe((data) => {
+      this.contacts = data;
+    });
+  }
+
+  updateContact(contact: Contact) {
+    this.contactService.updateContact(contact);
+  }
+
+  add(name: string, phoneNumber: string): void {
+    name = name.trim();
+    phoneNumber = phoneNumber.trim();
+    if (!name) {
+      return;
     }
+    this.contactService
+      .addContact({ name, phoneNumber } as Contact)
+      .subscribe((contact) => {
+        this.contacts.push(contact);
+      });
+  }
 
-    getContacts(): void {
-        this.contactService.getContacts().subscribe(data => {
-            this.contacts = data;
-        });
-    }
-
-    updateContact(contact: Contact) {
-        this.contactService.updateContact(contact);
-    }
-
-    add(name: string, phoneNumber: string): void {
-        name = name.trim();
-        phoneNumber = phoneNumber.trim();
-        if (!name) { return; }
-        this.contactService.addContact({ name, phoneNumber } as Contact)
-            .subscribe(contact => {
-                this.contacts.push(contact);
-            });
-    }
-
-    delete(contact: Contact): void {
-        this.contactService.deleteContact(contact.id).subscribe(() => {
-            this.contacts = this.contacts.filter(c => c !== contact);
-        });
-    }
+  delete(contact: Contact): void {
+    this.contactService.deleteContact(contact.id).subscribe(() => {
+      this.contacts = this.contacts.filter((c) => c !== contact);
+    });
+  }
 }
